983249765b00a21dc089c896a566cecdffdf15ba
ByteOtter <christopher.hock@suse.com>
2023-03-07 17:12:51 +0100

    reformat cbl_tftp.py with black

--- src/cbl_tftp.py
+++ src/cbl_tftp.py
@@ -11,19 +11,19 @@ from fbtftp.base_handler import BaseHandler
 from fbtftp.base_server import BaseServer
 
 # debug values
-LISTEN_ON = '0.0.0.0'
+LISTEN_ON = "0.0.0.0"
 SERVER_PORT = 69
-TFTP_ROOT = 'example_configs/'
+TFTP_ROOT = "example_configs/"
 RETRIES = 3
 TIMEOUT = 5
-TEMPLATES_PATH = 'example_configs/templates/'
+TEMPLATES_PATH = "example_configs/templates/"
+
 
 class TftpData:
-
     def __init__(self, filename):
         path = os.path.join(TFTP_ROOT, filename)
         self._size = os.stat(path).st_size
-        self._reader = open(path, 'rb')
+        self._reader = open(path, "rb")
 
     def read(self, data):
         return self._reader.read(data)
@@ -34,36 +34,37 @@ class TftpData:
     def close(self):
         self._reader.close()
 
-class StaticHandler(BaseHandler):
 
+class StaticHandler(BaseHandler):
     def get_response_data(self):
         return TftpData(self._path)
 
-class TftpServer(BaseServer):
 
+class TftpServer(BaseServer):
     def get_handler(self, server_addr, peer, path, options):
-        return StaticHandler(
-            server_addr, peer, path, options, session_stats
-        )
+        return StaticHandler(server_addr, peer, path, options, session_stats)
+
 
 # render config template
 def render_template(template, **kwargs):
     env = Environment(
-        loader = FileSystemLoader(TEMPLATES_PATH),
-        undefined = StrictUndefined,
-        trim_blocks = True
-        )
+        loader=FileSystemLoader(TEMPLATES_PATH),
+        undefined=StrictUndefined,
+        trim_blocks=True,
+    )
 
     template = env.get_template(template)
     return template.render(**kwargs)
 
+
 def session_stats(stats):
-    print('')
-    print('#' * 60)
-    print('Peer: {} UDP/{}'.format(stats.peer[0], stats.peer[1]))
-    print('File: {}'.format(stats.file_path))
-    print('Sent Packets: {}'.format(stats.packets_sent))
-    print('#' * 60)
+    print("")
+    print("#" * 60)
+    print("Peer: {} UDP/{}".format(stats.peer[0], stats.peer[1]))
+    print("File: {}".format(stats.file_path))
+    print("Sent Packets: {}".format(stats.packets_sent))
+    print("#" * 60)
+
 
 def main():
     server = TftpServer(LISTEN_ON, SERVER_PORT, RETRIES, TIMEOUT)
@@ -72,5 +73,6 @@ def main():
     except KeyboardInterrupt:
         server.close()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
