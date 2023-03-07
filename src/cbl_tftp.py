440edfddff9ec0e940e0eaceaf983a26862da035
ByteOtter <christopher.hock@suse.com>
2023-03-07 14:43:59 +0100

    fix ci errors

--- src/cbl_tftp.py
+++ src/cbl_tftp.py
@@ -1,9 +1,13 @@
 # This source code is licensed under the MIT license
 
-from fbtftp.base_handler import BaseHandler
-from fbtftp.base_server import BaseServer
+"""
+tftp server with static file handling
+"""
 
 import os
+
+from fbtftp.base_handler import BaseHandler
+from fbtftp.base_server import BaseServer
 
 LISTEN_ON = '0.0.0.0'
 SERVER_PORT = 69
@@ -12,33 +16,33 @@ RETRIES = 3
 TIMEOUT = 5
 
 class TftpData:
-    
+
     def __init__(self, filename):
         path = os.path.join(TFTP_ROOT, filename)
         self._size = os.stat(path).st_size
         self._reader = open(path, 'rb')
-    
+
     def read(self, data):
         return self._reader.read(data)
-    
+
     def size(self):
         return self._size
-    
+
     def close(self):
         self._reader.close()
 
 class StaticHandler(BaseHandler):
-    
+
     def get_response_data(self):
         return TftpData(self._path)
 
 class TftpServer(BaseServer):
-    
+
     def get_handler(self, server_addr, peer, path, options):
         return StaticHandler(
             server_addr, peer, path, options, session_stats
         )
-    
+
 def session_stats(stats):
     print('')
     print('#' * 60)
